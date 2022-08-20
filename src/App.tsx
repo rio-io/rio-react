e05abb439d78fe1d9cff3641a1d633b3481d1602
Me1e <mele0404@naver.com>
2022-08-20 18:20:21 +0900

    라우트 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Main>
+      <Link to={"/resume"}>Personal</Link>
+      <Link to={"/stamp"}>Organization</Link>
+      <Link to={"/view"}>View</Link>
+    </Main>
+  );
 }
+
+const Main = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  gap: 10vh;
+  flex-direction: column;
+`;
 
 export default App;
