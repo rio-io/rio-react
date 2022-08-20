5e0084742e8b2ade3236d85a1a508050b9c5a582
Me1e <mele0404@naver.com>
2022-08-20 18:02:58 +0900

    init

---
+++ src/components/Stamp.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Stamp() {
+  return <div>stamp</div>;
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Resume from "./components/Resume";
+import Stamp from "./components/Stamp";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}></Route>
+      <Route path="/Resume" element={<Resume />}></Route>
+      <Route path="/stamp" element={<Stamp />}></Route>
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
