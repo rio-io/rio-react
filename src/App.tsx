e76e26ca752cfad853f7c0e3b945cba3d747162f
Junhyung Park <junhyung1821@gmail.com>
2022-08-20 07:30:23 -0600

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import Header from './components/Header'
 
 export default function App() {
   return (
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function Header() {
+    return (
+        <>
+            <Bar>
+                <Icon src={require('../images/rio_icon.png')} />
+            </Bar>
+        </>
+    );
+}
+
+const Bar = styled.div`
+    width: 428px;
+    height: 134px;
+    left: 0px;
+    background: #241C55;
+`;
+
+const Icon = styled.img`
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 70px;
+`
