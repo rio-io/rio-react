15feda54471d30c2bf52334fdf893d4561739fbb
Junhyung Park <junhyung1821@gmail.com>
2022-08-20 05:16:15 -0600

    stamp form

--- src/components/Stamp.tsx
+++ src/components/Stamp.tsx
@@ -1,5 +1,26 @@
 import React from "react";
 
 export default function Stamp() {
-  return <div>stamp</div>;
+  return (
+    <>
+        <div>
+            <h1>
+                Org Name
+            </h1>
+        </div>
+        <div>
+            <form action="/send-data-here" method="post">
+                <label>Event:</label>
+                <input type="text" id="event" name="event"/>
+                <label>Receiver's Wallet ID:</label>
+                <input type="text" id="last" name="last"/>
+                <select>
+                    <option value="competition">Competition</option>
+                    <option value="work">Work</option>
+                </select>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    </>
+  )
 }
